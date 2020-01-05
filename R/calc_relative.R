6d7ddf75fea867c491bb5a3339503edc768201ec
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-01-05 12:40:49 -0600

    relative criteria

---
+++ R/calc_relative.R
@@ -0,0 +1,38 @@
+#' Calculate performance criteria and MCSE.
+#'
+#' @param estimates A numeric vector including the estimates.
+#' @param true_parameter A number indicating the true parameter.
+#' @param K A number indicating number of simulation iterations.
+#' @param perfm_criteria A character or a character vector indicating the performance criteria to be calculated.
+#'
+#' @return The performance criteria estimate and the associated MCSE.
+
+
+#' @export
+calc_relative <- function(estimates, true_param, K, perfm_criteria = c("relative bias", "relative mse")){
+
+  #calculate sample stats
+  t_bar <- mean(estimates)
+  bias <- t_bar - true_param
+  var_t <- var(estimates)
+  s_t <- sd(estimates)
+  k_t <- (1/(K * s_t^4)) * sum((estimates - t_bar)^4)
+  g_t <- (1/(K * s_t^3)) * sum((estimates - t_bar)^3)
+  mse <- bias^2 + var_t
+
+  # initialize data frame
+  dat <- data.frame(matrix(ncol = 0, nrow = 1))
+
+  if("relative bias" %in% perfm_criteria){
+    dat$rel_bias <- t_bar / true_param
+    dat$rel_bias_mcse <- sqrt(var_t / (K * true_param^2))
+  }
+
+  if("relative mse" %in% perfm_criteria){
+    dat$rel_mse <- mse / (true_param^2)
+    dat$rel_mse_mcse <- sqrt((1 / K * true_param^2) * (s_t^4 * (k_t -1) + 4 * s_t^3 * g_t * bias + 4 * var_t * bias^2))
+  }
+
+  return(dat)
+
+}
