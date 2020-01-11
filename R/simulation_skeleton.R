087ed5c488d114724d027f2ee33a91616e06c6cb
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-01-10 18:47:30 -0600

    sim skeleton from Pusto

---
+++ R/simulation_skeleton.R
@@ -0,0 +1,50 @@
+#' @title Open an APA-6 style .Rnw template
+#'
+#' @description
+#' Creates and opens an .Rnw file containing a template for writing a manuscript in APA-6 style.
+#'
+#' @param filename Name of file to create.
+#' @param ... Further arguments passed to file.copy
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' Rnw_APA6("Awesome-New-Paper")
+#' }
+#'
+
+
+Rnw_APA6 <- function(filename, ...) {
+
+  from <- system.file("templates","APA6.Rnw",package="Pusto")
+  to <- paste0(gsub(" ","-",filename),".Rnw")
+  copy <- file.copy(from, to, ...)
+
+  return(utils::file.edit(to))
+}
+
+
+#' @title Open a simulation skeleton
+#'
+#' @description
+#' Creates and opens an .R file containing a skeleton for writing a Monte Carlo simulation study.
+#'
+#' @param filename Name of file to create.
+#' @param ... Further arguments passed to file.copy
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' Simulation_Skeleton("Behrens-Fisher problem")
+#' }
+
+simulation_skeleton <- function(filename, ...) {
+
+  from <- system.file("templates","Simulation-Skeleton.R",package="Pusto")
+  to <- paste0(filename,".R")
+  copy <- file.copy(from, to, ...)
+
+  return(utils::file.edit(to))
+}
