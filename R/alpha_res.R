2a035c892382acb529d704d7904de1a64cb3a547
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-02-21 20:32:38 -0600

    documented datasets

---
+++ R/alpha_res.R
@@ -0,0 +1,14 @@
+#' Cronbach's alpha Simulation Results
+#'
+#' A dataset containing simulation results estimating Cronbach's alpha and it's variance
+#'
+#'
+#' @format A tibble with 1000 rows and 3 variables:
+#' \describe{
+#'   \item{A}{estimate of alpha}
+#'   \item{Var_A}{variance of alpha}
+#'   \item{true_param}{true alpha used to generate the data}
+#'
+#' }
+#'
+"alpha_res"
