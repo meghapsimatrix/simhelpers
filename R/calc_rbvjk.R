a8e24b2b92942c8fa1aedf8329d927c6b6da1da2
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-02-26 10:06:34 -0600

    Checking jacknife formula and updating the vignette

--- R/calc_rbvjk.R
+++ R/calc_rbvjk.R
@@ -31,9 +31,12 @@ calc_rbvjk <- function(res_dat, estimates, var_estimates, perfm_criteria = c("re
   t_bar <- mean(estimates) # sample mean of the estimates
   var_t <- var(estimates) # sample variance of the estiates
   var_v <- var(var_est)
+
+  # jacknife
   v_bar_j <- (1 / (K - 1)) * (K * v_bar - var_est)
   s_sq_t_j <- (1 / (K - 2)) * ((K - 1) * var_t - (K / (K - 1)) * (estimates - t_bar)^2)
   s_sq_v_j <- (1 / (K - 2)) * ((K - 1) * var_v - (K / (K - 1)) * (var_est - v_bar)^2)
+
   rb_var <- v_bar/ var_t
   rel_mse_var_j <- ((v_bar_j - s_sq_t_j)^2 + s_sq_v_j)/(s_sq_t_j)^2
 
@@ -51,7 +54,7 @@ calc_rbvjk <- function(res_dat, estimates, var_estimates, perfm_criteria = c("re
 
   if("relative mse" %in% perfm_criteria){
     dat <- dat %>%
-      dplyr::mutate(rel_mse_var = ((v_bar - var_t)^2 + var_v)/  var_t^2,
+      dplyr::mutate(rel_mse_var = ((v_bar - var_t)^2 + var_v) /  var_t^2,
                     rel_mse_var_mcse = sqrt((1/K) * sum((rel_mse_var_j - rel_mse_var)^2)),
                     rel_mse_var = dplyr::if_else(var_t == 0, as.numeric(NA), rel_mse_var),
                     rel_mse_var_mcse = dplyr::if_else(var_t == 0, as.numeric(NA), rel_mse_var_mcse))
@@ -59,7 +62,7 @@ calc_rbvjk <- function(res_dat, estimates, var_estimates, perfm_criteria = c("re
 
   if("relative rmse" %in% perfm_criteria){
     dat <- dat %>%
-      dplyr::mutate(rel_rmse_var = sqrt(((v_bar - var_t)^2 + var_v)/  var_t^2),
+      dplyr::mutate(rel_rmse_var = sqrt(((v_bar - var_t)^2 + var_v) /  var_t^2),
                     rel_rmse_var_mcse = sqrt((1/K) * sum((sqrt(rel_mse_var_j) - rel_rmse_var)^2)),
                     rel_rmse_var = dplyr::if_else(var_t == 0, as.numeric(NA), rel_rmse_var),
                     rel_rmse_var_mcse = dplyr::if_else(var_t == 0, as.numeric(NA), rel_rmse_var_mcse))
