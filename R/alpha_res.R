e127504643fade5abdae3915e965680cedadf40d
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-03-14 13:29:14 -0500

    documentation for datasets

--- R/alpha_res.R
+++ R/alpha_res.R
@@ -1,13 +1,13 @@
 #' Cronbach's alpha Simulation Results
 #'
-#' A dataset containing simulation results from estimating Cronbach's alpha and its variance
+#' A dataset containing simulation results from estimating Cronbach's alpha and its variance.
 #'
 #'
 #' @format A tibble with 1000 rows and 3 variables:
 #' \describe{
-#'   \item{A}{estimate of alpha}
-#'   \item{Var_A}{estimate of the variance of alpha}
-#'   \item{true_param}{true alpha used to generate the data}
+#'   \item{A}{estimate of alpha.}
+#'   \item{Var_A}{estimate of the variance of alpha.}
+#'   \item{true_param}{true alpha used to generate the data.}
 #'
 #' }
 #'
--- R/welch_res.R
+++ R/welch_res.R
@@ -1,20 +1,20 @@
 #' Welch t-test Simulation Results
 #'
-#' A dataset containing simulation results comparing Welch t-test to normal t-test
+#' A dataset containing simulation results from a study comparing Welch t-test to normal t-test.
 #'
 #'
 #' @format A tibble with 24000 rows and 10 variables:
 #' \describe{
-#'   \item{n}{sample size per group assuming equal sample size}
-#'   \item{mean_diff}{true difference in means of two groups used to generate the data}
-#'   \item{iterations}{number of iterations}
-#'   \item{seed}{seed used to generate data}
-#'   \item{method}{indicates whether Welch or normal t-test was used}
-#'   \item{est}{estimate of the mean difference}
-#'   \item{var}{variance of the estimate}
-#'   \item{p_val}{p-value from the t-test}
-#'   \item{lower_bound}{lower bound of the confidence interval}
-#'   \item{upper_bound}{upper bound of the confidence interval}
+#'   \item{n}{sample size per group assuming equal sample size.}
+#'   \item{mean_diff}{true difference in means of two groups used to generate the data.}
+#'   \item{iterations}{number of iterations.}
+#'   \item{seed}{seed used to generate data.}
+#'   \item{method}{indicates whether Welch or normal t-test was used.}
+#'   \item{est}{estimate of the mean difference.}
+#'   \item{var}{variance of the estimate.}
+#'   \item{p_val}{p-value from the t-test.}
+#'   \item{lower_bound}{lower bound of the confidence interval.}
+#'   \item{upper_bound}{upper bound of the confidence interval.}
 #'
 #' }
 #'
