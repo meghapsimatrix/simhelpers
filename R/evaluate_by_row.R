6fa0f6f87a4d9149e67d444bad51f5b9857cea83
jepusto <jepusto@gmail.com>
2024-01-08 21:25:59 -0600

    evaluate_by_row() gains an argument to control the name of the variable where simulation results are stored.

--- R/evaluate_by_row.R
+++ R/evaluate_by_row.R
@@ -11,6 +11,7 @@
 #'   the variable names in \code{params}. The function must return a
 #'   \code{data.frame}, \code{tibble}, or vector.
 #' @param ... additional arguments passed to \code{sim_function}.
+#' @param results_name character string to set the name of the column storing the results of the simulation. Default is \code{".results"}.
 #' @param system_time logical indicating whether to print computation time.
 #'   \code{TRUE} by default.
 #' @inheritParams furrr::future_pmap
@@ -29,6 +30,7 @@
 #'
 
 evaluate_by_row <- function(params, sim_function, ...,
+                            results_name = ".results",
                             .progress = FALSE, .options = furrr::furrr_options(),
                             system_time = TRUE) {
 
@@ -44,7 +46,7 @@ evaluate_by_row <- function(params, sim_function, ...,
 
   if (system_time) print(sys_tm, "\n")
 
-  params$.results <- results_list
-  tidyr::unnest(params, cols = .results)
+  params[[results_name]] <- results_list
+  tidyr::unnest(params, cols = tidyr::all_of(results_name))
 
 }
--- tests/testthat/test_ebr.R
+++ tests/testthat/test_ebr.R
@@ -1,13 +1,27 @@
+library(dplyr)
+library(tidyr)
+
 df <- data.frame(
   a = 3:5,
   b= seq(8, 16, 4)
 )
 
 test_that("evaluate_by_row works", {
+  f <- function(a, b) a * b
   expect_equal(
-    evaluate_by_row(df, function(a, b) a * b) %>% rename(res = .results),
+    evaluate_by_row(df, f, results_name = "res"),
     df %>%
-      mutate(res = future_pmap(., .f = function(a, b) a * b)) %>%
-      unnest(cols = res))
+      mutate(res = pmap(., .f = f)) %>%
+      unnest(cols = res)
+  )
+
+  g <- function(a, b) data.frame(x = a * b, y = a + b)
+  expect_equal(
+    evaluate_by_row(df, g, results_name = "res"),
+    df %>%
+      mutate(res = pmap(., .f = g)) %>%
+      unnest(cols = res)
+  )
+
 })
 
