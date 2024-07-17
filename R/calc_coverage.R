7f5637db79a43e1692a5efce46619613c861bec4
jepusto <jepusto@gmail.com>
2024-07-17 16:34:41 -0500

    Working on function to extrapolate coverage and width of bootstrap confidence intervals.

--- R/calc_coverage.R
+++ R/calc_coverage.R
@@ -72,3 +72,86 @@ calc_coverage <- function(
   return(dat)
 
 }
+
+
+#' @title Extrapolate coverage and width using sub-sampled bootstrap confidence
+#'   intervals.
+#'
+#' @description Given a set of bootstrap confidence intervals calculated across
+#'   sub-samples with different numbers of replications, extrapolates confidence
+#'   interval coverage and width of bootstrap confidence intervals to a
+#'   specified (larger) number of bootstraps. The function also calculates the
+#'   associated Monte Carlo standard errors. The confidence interval percentage
+#'   is based on how you calculated the lower and upper bounds.
+#'
+#' @param CI_subsamples list or name of column from \code{data} containing list
+#'   of confidence intervals calculated based on sub-samples with different
+#'   numbers of replications.
+#' @param criteria character or character vector indicating the performance
+#'   criteria to be calculated, with possible options \code{"coverage"} and
+#'   \code{"width"}.
+#' @inheritParams calc_absolute
+#'
+#' @return A tibble containing the number of simulation iterations, performance
+#'   criteria estimate(s) and the associated MCSE.
+#'
+#' @export
+#'
+#' @examples
+#' project_coverage(
+#'   data = t_boots,
+#'   CI_subsamples = CIs,
+#'   true_param = true_param
+#' )
+#'
+
+project_coverage <- function(
+    data,
+    CI_subsamples,
+    true_param,
+    criteria = c("coverage", "width"),
+    winz = Inf
+) {
+
+  criteria <- match.arg(criteria, choices = c("coverage", "width"), several.ok = TRUE)
+
+  if (!missing(data)) {
+    cl <- match.call()
+    true_param <- eval(cl$true_param, envir = data, enclos = parent.frame())
+    CI_subsamples <- eval(cl$CI_subsamples, envir = data, enclos = parent.frame())
+  }
+
+  true_param <- unique(true_param) # true param
+  if (length(true_param) > 1L) stop("`true_param` must have a single unique value.")
+
+  not_miss <- !is.na(lower_bound) & !is.na(upper_bound)
+  lower_bound <- lower_bound[not_miss]
+  upper_bound <- upper_bound[not_miss]
+
+  K <- length(CI_subsamples) # iterations
+  width <- upper_bound - lower_bound
+
+  if (winz < Inf) width <- winsorize(width, winz)
+
+  # initialize tibble
+  dat <- tibble::tibble(K_coverage = K)
+
+  if (winz < Inf) {
+    dat$width_winsor_pct <- attr(width, "winsor_pct")
+    dat$width_winsor_pct_mcse <- sqrt(dat$width_winsor_pct * (1 - dat$width_winsor_pct) / K)
+  }
+
+  if ("coverage" %in% criteria) {
+    coverage <- mean(lower_bound <= true_param & true_param <= upper_bound)
+    dat$coverage <- coverage
+    dat$coverage_mcse = sqrt(coverage * (1 - coverage) / K)
+  }
+
+  if ("width" %in% criteria) {
+    dat$width <- mean(width)
+    dat$width_mcse <- sqrt(var(width) / K)
+  }
+
+  return(dat)
+
+}
